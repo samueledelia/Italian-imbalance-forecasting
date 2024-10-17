20f6d4009ba0c68a1299174cd600694991f9ff18
Samuele D'Elia <delia.samuele00@gmail.com>
2024-10-17 14:46:54 +0200

    fourth commit

--- src/utils.py
+++ src/utils.py
@@ -21,8 +21,26 @@ def filter_and_rename_energy_source(df, energy_source, new_column_name):
     filtered_df.drop(columns=['ENERGY_SOURCE'], inplace=True)
     
     # Rename the renewable generation column
-    filtered_df.rename(columns={'RENEWABLE_GENERATION_GWh': new_column_name}, inplace=True)
+    filtered_df.rename(columns={'ENERGY_BALANCE_GWh': new_column_name}, inplace=True)
     
     return filtered_df
 
 
+def filter_load_by_zona(df, zonas):
+    """
+    Filters the DataFrame to include only rows where the 'ZONA' is in the provided list of zonas.
+
+    Parameters:
+        df (pd.DataFrame): The input DataFrame containing load data.
+        zonas (list): A list of 'ZONA' values to filter by.
+
+    Returns:
+        pd.DataFrame: A filtered DataFrame containing only the specified 'ZONA' values.
+    """
+    # Filtering the DataFrame
+    filtered_df = df[df['ZONA'].isin(zonas)]
+    return filtered_df
+
+
+def filter_scheduled_foreign_exchange(df, country):
+    return df[df['COUNTRY'].str.lower() == country.lower()]
