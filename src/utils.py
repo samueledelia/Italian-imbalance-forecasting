3b1f8b6b5aee9d0ae538e3770e94c1236aeff439
Samuele D'Elia <delia.samuele00@gmail.com>
2024-11-26 12:48:47 +0100

    add imbalance forecast notebook

--- src/utils.py
+++ src/utils.py
@@ -62,3 +62,28 @@ def process_foreign_exchange(df, country):
     country_df[f'{country.upper()}_MWQH'] = country_df[f'{country.upper()}_MWQH'] / 4
     
     return country_df
+
+
+def filter_and_rename_energy_type(df, energy_source, new_column_name):
+    """
+    Filters the DataFrame by the specified energy source, drops the ENERGY_SOURCE column, 
+    and renames the renewable generation column.
+
+    Parameters:
+    - df (pd.DataFrame): The DataFrame containing renewable energy data.
+    - energy_source (str): The energy source to filter (e.g., 'Wind').
+    - new_column_name (str): The new name for the renewable generation column.
+
+    Returns:
+    - pd.DataFrame: A new DataFrame with the filtered data and renamed column.
+    """
+    # Filter the DataFrame for the specified energy source and create a copy
+    filtered_df = df[df['ENERGY_TYPE'] == energy_source].copy()
+    
+    # Drop the ENERGY_SOURCE column
+    filtered_df.drop(columns=['ENERGY_TYPE'], inplace=True)
+    
+    # Rename the renewable generation column
+    filtered_df.rename(columns={'VALUE': new_column_name}, inplace=True)
+    
+    return filtered_df
